479d206a30e2b97496cec709001aa7e0932adc2b
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-01 18:47:19 +0300

    move to chat component

--- mobile/App.js
+++ mobile/App.js
@@ -1,59 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { Chat } from "./screens/Chat";
 
-import { useEffect, useState } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-import { io } from 'socket.io-client';
 
-const socket = io("http://127.0.0.1:3000")
+const Stack = createNativeStackNavigator()
 
 const App = () => {
 
-  const [chatMessage, setChatMessage] = useState("")
-  const [chatMessages, setChatMessages] = useState([])
-
-  const submitChatMessage = () => {
-    socket.emit("chat message", chatMessage)
-    setChatMessage("")
-  }
-
-  useEffect(() => {
-    socket.on("chat message", msg => {
-      setChatMessages([...chatMessages, msg])
-    })
-  }, [])
-
   return (
-    <SafeAreaView>
-      <TextInput
-        value={chatMessage}
-        onChangeText={text => setChatMessage(text)}
-        onSubmitEditing={() => submitChatMessage()}
-        style={styles.textInput}
-      />
-
-      {
-        chatMessages?.map(message => {
-          return (
-            <View key={message}>
-              <Text>{message}</Text>
-            </View>
-          )
-        })
-      }
-    </SafeAreaView>
-  );
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
 };
-
-const styles = StyleSheet.create({
-  textInput: {
-    height: 40,
-    borderWidth: 2
-  },
-});
 
 export default App;
---
+++ mobile/screens/Chat.js
@@ -0,0 +1,63 @@
+
+import { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { io } from 'socket.io-client';
+import { setChatMessages } from '../redux/chatSlice';
+
+const socket = io("http://127.0.0.1:3000")
+
+export const Chat = () => {
+
+  const chatMessages = useSelector(state => state.chat.chatMessages)
+  const dispatch = useDispatch()
+  const [chatMessage, setChatMessage] = useState("")
+  const [her, setHer] = useState("")
+
+  const submitChatMessage = () => {
+    socket.emit("chat message", chatMessage)
+    setChatMessage("")
+  }
+
+  useEffect(() => {
+    socket.on("chat message", async (message) => {
+      const msg = await message
+      dispatch(setChatMessages(msg))
+    })
+  }, [])
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        value={chatMessage}
+        onChangeText={text => setChatMessage(text)}
+        onSubmitEditing={() => submitChatMessage()}
+        style={styles.textInput}
+      />
+
+      {
+        chatMessages?.map(message => {
+          return (
+            <View key={message}>
+              <Text>{message}</Text>
+            </View>
+          )
+        })
+      }
+    </SafeAreaView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    borderWidth: 2
+  },
+});
